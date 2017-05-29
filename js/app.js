6de25107d9f468421f1e158f8a1309f58b1762c7
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-05-29 16:58:05 +0000

    corrigiendo corto de flechitas y botones ya sirven las dos

--- js/app.js
+++ js/app.js
@@ -1,38 +1,11 @@
-$(document).ready(function() {
-  $(".nextLink").click(function(e){
-    var currentActiveImage = $(".img-shown");
-    var nextActiveImage = currentActiveImage.next();
-    
-    if(nextActiveImage.length == 0 ) {
-      nextActiveImage = $(".carrousel-inner img").first();
-    }
-    
-    currentActiveImage.removeClass("img-shown").addClass("img-hidden").css("z-index", -10);
-    nextActiveImage.addClass("img-shown").removeClass("img-hidden").css("z-index", 20);
-    $(".carrousel-inner img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
-    e.preventDefault();
-  });
-  $(".previousLink").click(function(e) {
-    var currentActiveImage = $(".img-shown");
-    var previousActiveImage = currentActiveImage.prev();
-    
-    if(previousActiveImage.length == 0){
-      previousActiveImage = $(".carrousel-inner img").last();
-    }
-    
-    currentActiveImage.removeClass("img-shown").addClass("img-hidden").css("z-index", -10);
-    previousActiveImage.addClass("img-shown").removeClass("img-hidden").css("z-index", 20);
-    $(".carrousel-inner img").not([currentActiveImage, previousActiveImage]).css("z-index", 1);
-    e.preventDefault();
-  });
-  
+$(document).ready(function() {  
   $("#uno").click(function(){
     $("#1").addClass("img-shown").removeClass("img-hidden");
     $("#2").removeClass("img-shown").addClass("img-hidden");
     $("#3").removeClass("img-shown").addClass("img-hidden");
     $("#4").removeClass("img-shown").addClass("img-hidden");
     $("#5").removeClass("img-shown").addClass("img-hidden");
-  })
+  });
   
   $("#dos").click(function() {
     $("#2").addClass("img-shown").removeClass("img-hidden");
@@ -65,7 +38,32 @@ $(document).ready(function() {
     $("#4").removeClass("img-shown").addClass("img-hidden");
   });
   
-  
+  $(".nextLink").click(function(e){
+    var currentActiveImage = $(".img-shown");
+    var nextActiveImage = currentActiveImage.next();
+    
+    if(nextActiveImage.length == 0 ) {
+      nextActiveImage = $(".carrousel-inner img").first();
+    }
+    
+    currentActiveImage.removeClass("img-shown").addClass("img-hidden").css("z-index", -10);
+    nextActiveImage.addClass("img-shown").removeClass("img-hidden").css("z-index", 20);
+    $(".carrousel-inner img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+    e.preventDefault();
+  });
+  $(".previousLink").click(function(e) {
+    var currentActiveImage = $(".img-shown");
+    var previousActiveImage = currentActiveImage.prev();
+    
+    if(previousActiveImage.length == 0){
+      previousActiveImage = $(".carrousel-inner img").last();
+    }
+    
+    currentActiveImage.removeClass("img-shown").addClass("img-hidden").css("z-index", -10);
+    previousActiveImage.addClass("img-shown").removeClass("img-hidden").css("z-index", 20);
+    $(".carrousel-inner img").not([currentActiveImage, previousActiveImage]).css("z-index", 1);
+    e.preventDefault();
+  });
 });
 
 
